13fa7da280a1db9f3c9248d148a01cb4392e2c11
Piper Jarrett <piperjarrett@pipers-mbp.lan>
2022-09-23 09:41:11 -0600

    add successful fetch call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,15 +1,16 @@
 function fetchData(details) {
   return fetch(`http://localhost:3001/api/v1/${details}`)
     .then((response) => response.json())
-    .then((data) => console.log(data))
     .catch((error) => console.log(error));
 }
 
-const promisAll = () => {
-  result = Promise.all([fetchData("travelers"), fetchData("trips")]).then(
-    (reponse) => {
+const promiseAll = () => {
+  const result = Promise.all([fetchData("travelers"), fetchData("trips")]).then(
+    (response) => {
       return response;
     }
   );
   return result;
 };
+
+export { promiseAll };
--- src/scripts.js
+++ src/scripts.js
@@ -2,10 +2,24 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+import "./css/styles.css";
+import { promiseAll } from "./apiCalls.js";
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+import "./images/down-arrow.svg";
 
+//Gloabl Variables
+let travelData;
+let tripsData;
+let traveler;
 
-console.log('This is the JavaScript entry file - your code begins here.');
+//Event Listeners
+window.addEventListener("load", promiseAll);
+
+//Functions
+
+promiseAll().then((responses) => {
+  travelData = responses[0].travelers;
+  tripsData = responses[1].trips;
+  traveler = new Traveler();
+});
